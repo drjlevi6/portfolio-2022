f6d820f5fa7e5b839bc6fcf111d9f56569c3dcf0
drjlevi6 <drjlevi6@aol.com>
2022-07-30 23:28:25 -0400

    Added folders python/python-geometrical/angle-bisector-theorem plus intermediate folders; Python modules perpBisectors.py, perpView.py

---
+++ python/python-geometrical/angle-bisector-theorem/perpBisectors.py
@@ -0,0 +1,51 @@
+""" Draws a triangle, its sides' perpendicular bisectors and circumcircle """
+
+import math, sys
+import turtle
+import perpView
+
+# Point-slope equation for BC perpendicular bisector
+def get_bisector_BC(x):
+	return slope_perp_BC * (x - midBCx) + midBCy
+
+# Compute triangle data:
+# vertices
+Ax, Ay = [0.0, 0.0]
+Bx, By = [600.0, 1.0]	# Make ordinate By a tiny pos int so that slope of normal != ∞
+Cx, Cy = [150, 400.0]
+vertices = [ [Ax, Ay], [Bx, By], [Cx, Cy] ]
+
+# Midpoints:
+midABx, midABy = [ (Ax+Bx)/2, (Ay+By)/2]
+midBCx, midBCy = [ (Bx+Cx)/2, (By+Cy)/2]
+midCAx, midCAy = [ (Cx+Ax)/2, (Cy+Ay)/2]
+midpoints = [[midABx, midABy], [midBCx, midBCy ], [midCAx, midCAy]]
+
+# Slope of BC and its perpendicular bisector:
+slope_BC = (Cy-By)/(Cx-Bx)
+slope_perp_BC = -1/slope_BC
+
+
+# Make a different definition of slopes of both triangle's edges and their normals:
+slopes = []
+
+for i in range(3):
+	slopes.append( 
+		( vertices[ (i+1)%3 ][1] - vertices[i][1] ) / 
+			( vertices[ (i+1)%3 ][0] - vertices[i][0] )
+	)
+	"""
+	print("i =", i)
+	print("Ordinates:", vertices[ (i+1)%3 ][1], vertices[i][1], ":", 
+		( vertices[ (i+1)%3 ][1] - vertices[i][1] ) )
+	print("Abcissas:", vertices[ (i+1)%3 ][0], vertices[i][0], ":", 
+		( vertices[ (i+1)%3 ][0] - vertices[i][0] ) )
+		"""
+
+# Circumcenter, calculated using get_bisector_BC():
+circ_center_x, circ_center_y = [ midABx, get_bisector_BC(midABx) ]
+circumradius = math.sqrt(circ_center_x**2 + circ_center_y**2)
+circle_data = [[circ_center_x, circ_center_y], circumradius]
+
+# VIEW: Use module turtle; also plan an alternative display using Ghostscript.
+perpView.show_view_with_turtle(vertices, midpoints, slopes, circle_data)
---
+++ python/python-geometrical/angle-bisector-theorem/perpView.py
@@ -0,0 +1,183 @@
+""" Handles the View aspect of the Perpendicular Bisectors program, 
+using Python's Turtle module. (Another module is planned, to present 
+the same view using GhostScript.)"""
+
+import turtle
+from turtle import *
+import math
+
+global llx, lly, urx, ury
+llx, lly = [-45, -230]
+urx, ury = [llx + 700, lly + 700]
+
+def open_screen(s):
+	""" open the Turtle canvas (screen); set its coordinates"""
+	s.setworldcoordinates(llx, lly, urx, ury)
+
+def draw_triangle(t, vertices):
+	"""Draw the base triangle"""
+	[ [Ax, Ay], [Bx, By], [Cx, Cy] ] = vertices
+	t.speed(6)
+	t.goto(Ax, Ay)
+	t.speed(3)
+	t.pendown()
+	t.goto(Bx, By)
+	t.goto(Cx, Cy)
+	t.goto(Ax, Ay)
+
+def draw_bisectors(t, midpoints, circle_data):
+	"""Draw base triangle's parpenducular bisectors"""
+	[[midABx, midABy], [midBCx, midBCy ], [midCAx, midCAy]] = midpoints
+	[[circ_center_x, circ_center_y], circumradius] = circle_data
+	
+	t.pencolor( "blue" )
+	t.penup();		t.goto( circ_center_x, circ_center_y )
+	t.pendown();	t.goto( midABx, midABy )
+	t.penup();		t.goto( circ_center_x, circ_center_y )
+	t.pendown();	t.goto( midBCx, midBCy )
+	t.penup();		t.goto( circ_center_x, circ_center_y )
+	t.pendown();	t.goto( midCAx, midCAy )
+
+def draw_circumcircle( t, circle_data ):
+	"""Draw the base triangle's circumscribing circle"""
+	saved_speed = t.speed()
+	t.speed(10)
+	[[circ_center_x, circ_center_y], 
+		circumradius] = circle_data
+	t.pencolor( "red" )
+	t.penup();
+	t.goto( circ_center_x, circ_center_y )
+	t.setheading(math.pi )
+	t.pendown()
+	t.goto( circ_center_x - circumradius, circ_center_y );
+	t.setheading(math.pi * 1.5);
+	t.speed(3)
+	t.circle(circumradius)
+	t.speed(saved_speed)
+
+def draw_right_angles(midpoints, slopes):
+	""" Draws right angles between triangle's edges & their perp. bisectors"""
+	for i in range(2):
+		draw_one_right_angle(midpoints[i], slopes[i])
+
+def draw_ticks(s, t, vertices, midpoints, slopes):
+	"""Arranges drawing (two) groups of tick-marks on each triangle-edge. """
+	saved_width = t.width()
+	t.width(5)
+	saved_color = t.pencolor()
+	t.pencolor("purple")
+	for i in range(3):
+		draw_one_tick_group(s, t, i, vertices, slopes, 0.25)
+		draw_one_tick_group(s, t, i, vertices, slopes, 0.75)
+	t.pencolor(saved_color)
+	t.width(saved_width)
+
+def draw_one_tick_group(s, t, i, vertices, slopes, displacement):
+	"""Draws one group of (i) tick-marks on triangle edge i."""
+	""" The group is displaced by (decimal) argument displacement from 
+		starting vertex to ending vertex."""
+	# small = x if x < y else y
+	tick_group_width = 20.0	# width of group on triangle edge
+	tick_length = 20		# tick's total length (both sides of triangle edge)
+	t.width(1)
+	t.pencolor("black")
+	start_angle = math.atan(slopes[i]) if i==0 else \
+		math.atan(slopes[i]) + math.pi
+	t.setheading( start_angle )
+	s.delay(1)	# not 1000
+	t.penup()
+	group_center_x, group_center_y = [ 
+		(1.0-displacement) * vertices[i][0] + displacement * vertices[(i+1) % 3][0], 
+		(1.0-displacement) * vertices[i][1] + displacement * vertices[(i+1) % 3][1]
+		]
+	t.speed(6)
+	t.setheading(t.towards(group_center_x, group_center_y))
+	t.goto( group_center_x, group_center_y )
+	t.setheading( start_angle )
+	t.speed(3)
+	if i==0:
+		t.left(math.pi/2)
+		t.pendown()
+		t.forward(tick_length/2)
+		t.left(math.pi)
+		t.forward(tick_length)
+		t.penup()
+		t.left(math.pi)
+		t.forward(tick_length/2)
+	else:
+		t.backward(tick_group_width/i/2)
+		for j in range(i+1):
+			t.left(math.pi/2)
+			t.pendown()
+			t.forward(tick_length/2)
+			t.backward(tick_length)
+			t.penup()
+			t.forward(tick_length/2)
+			t.right(math.pi/2)
+			t.forward(tick_group_width/2)
+	
+def writeName(t, vertices):
+	"""Display author's name"""
+	t.pencolor("black")
+	t.penup()
+	t.speed(3)
+	t.setheading(t.towards(urx, lly + 100))
+	t.speed(6)
+	t.goto(urx, lly + 100)
+	t.pendown()
+	t.write("J LEVI 2021", True, align="right",
+		font=("Arial", 12, "normal"))	
+	t.penup()
+
+def draw_right_angles(t, midpoints, slopes):
+	""" Manages drawing a right-angle marker at the edges' midpoints"""
+	reverse_direction = [False, True, True]
+	saved_color = t.pencolor()
+	saved_speed = t.speed()
+	t.pencolor("green")
+	t.speed("slowest")
+	for i in range(3):
+		draw_one_right_angle(t, midpoints[i], slopes[i], 
+			reverse_direction[i])	
+	t.pencolor(saved_color)
+	t.speed(saved_speed)
+		
+def draw_one_right_angle(t, midpoint, slope, reverse_direction):
+	"""Draw a right-angle signs at the intersection of one of the triangle's 
+		edges and its perpendicular bisector."""
+	angle_side = 20 	# length of the right angle's sides
+	t.penup()
+	t.setheading(t.towards(midpoint[0], midpoint[1]))
+	t.speed(6)
+	t.goto(midpoint)
+	direction = math.atan(slope) if  not reverse_direction  else \
+		math.atan(slope) + math.pi
+	t.speed(3)
+	t.setheading(direction)	
+	t.forward(angle_side)
+	t.left(math.pi/2)
+	t.pendown()
+	t.forward(angle_side)
+	t.left(math.pi/2)
+	t.forward(angle_side)
+	t.penup()
+	
+
+### Main ######################################################################
+	
+def show_view_with_turtle(vertices, midpoints, slopes, circle_data):
+	""" Main function; calls others to draw the specific items """
+
+	turtle.setup( width=800, height=800 )
+	turtle.title("Perpendicular Bisectors of a Triangle")
+	t, s = [ Turtle(), Screen() ]
+	t.radians()
+	open_screen( s )
+	draw_triangle( t, vertices )
+	draw_bisectors( t, midpoints, circle_data )
+	draw_right_angles(t, midpoints, slopes)
+	draw_ticks(s, t, vertices, midpoints, slopes)
+	draw_circumcircle( t, circle_data )
+	writeName(t, vertices)
+
+
